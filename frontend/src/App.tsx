9494f0d9a76ee7679e790b9f25f38f3166709e6a
charukaonline <nuwanthakonline@gmail.com>
2025-07-09 21:33:18 +0530

    cors config and landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import DashboardLayout from "./components/ui/layout/DashboardLayout";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <div>
-      <DashboardLayout />
+      <Landing />
     </div>
   );
 }
